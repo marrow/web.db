b9c6eae4a98645f52227e57b4ac9248ba8011c52
Alice Bevan-McGregor <alice@gothcandy.com>
2016-09-20 00:32:11 -0400

    Ostensibly working MongoEngine adapter.

--- web/db/me.py
+++ web/db/me.py
@@ -1,10 +1,11 @@
 # encoding: utf-8
 
 import re
-from mongoengine import connect
 
-from marrow.package.loader import load
-from web.core.compat import native, items
+try:
+	from mongoengine import connect, disconnect
+except ImportError:
+	raise ImportError('Unable to import mongoengine; pip install mongoengine to fix this.')
 
 
 log = __import__('logging').getLogger(__name__)
@@ -12,28 +13,48 @@ log = __import__('logging').getLogger(__name__)
 _safe_uri_replace = re.compile(r'(\w+)://(\w+):(?P<password>[^@]+)@')
 
 
-class MongoEngineExtension:
-	__slots__ = ('uri', 'db', 'connection', 'cb')
+class MongoEngineDBConnection(object):
+	"""WebCore DBExtension interface for projects utilizing MongoEngine.
 	
-	provides = ['db']
+	URI-style connection strings should always be utilized. Provide a `replicaSet` query string argument to enable
+	replica set functionality. The alias name is preserved through to MongoEngine to allow use of database switching
+	mechanisms, such as the `switch_db` context manager, and multiple instances may be used to populate the
+	`DatabaseExtension` connections.
 	
-	def __init__(self, uri, **config):
-		log.info("Connecting MongoEngine to '%s'.", _safe_uri_replace.sub(r'\1://\2@', uri))
+	Any additional keyword arguments used are passed through to `mongoengine.connect`, which in turn passes the values
+	along to `mongoengine.connection.register_connection` for further processing.
+	"""
+	
+	def __init__(self, uri, alias=None, **kw):
+		"""Prepare configuration options."""
 		
-		self.uri = uri
-		self.cb = config.pop('ready', None)
-		self.connection = dict(config, host=uri, tz_aware=True)
+		kw['host'] = uri
 		
-		_, _, self.db = uri.rpartition('/')
+		self._alias = alias
+		self._config = kw
 	
 	def start(self, context):
-		db, connection = self.db, self.connection
+		"""Initialize the database connection."""
 		
-		context.mongoengine = connect(db, **connection)
+		# Only after passing to the DatabaseExtension intializer do we have a __name__...
+		name = self.name = self._alias or self.__name__
+		self._config['alias'] = name
 		
-		cb = self.cb
-		if cb is not None:
-			cb = load(cb) if isinstance(cb, native) else cb
-			
-			if hasattr(cb, '__call__'):
-				cb()
+		log.info("Connecting MongoEngine database layer.", extra=dict(
+				uri = _safe_uri_replace.sub(r'\1://\2@', self.uri),
+				config = self._config,
+			))
+		
+		self.connection = connect(**self._config)
+	
+	def prepare(self, context):
+		"""Attach this connection's default database to the context using our alias."""
+		
+		context.db[self.name] = self.connection.get_default_database()
+	
+	def stop(self, context):
+		"""Close the connection pool and clean up references in MongoEngine."""
+		
+		disconnect(self.name)
+		del self.connection
+
