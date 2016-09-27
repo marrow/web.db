69a245bde02eee1b25a8a74650d9e6afb182afae
Alice Bevan-McGregor <alice@gothcandy.com>
2016-09-26 22:58:16 -0400

    Documentation, Python 2 compatibility, cleaner code without explicit default.

--- web/db/release.py
+++ web/db/release.py
@@ -1,10 +1,9 @@
 # encoding: utf-8
 
-"""Release information about WebCore."""
+"""Release information about this WebCore add-on."""
 
 from __future__ import unicode_literals
 
-import sys
 from collections import namedtuple
 
 
@@ -15,3 +14,4 @@ author = namedtuple('Author', ['name', 'email'])("Alice Bevan-McGregor", 'alice@
 description = "General database adapter layer for common configuration and operation."
 copyright = "2009-2016, Alice Bevan-McGregor and contributors"
 url = 'https://github.com/marrow/web.db'
+
