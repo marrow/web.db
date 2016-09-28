bc3fe0ac522e42000757bc7409d40e24748d31c6
Alice Bevan-McGregor <alice@gothcandy.com>
2016-09-28 13:40:41 -0400

    Complete tests.

--- test/test_mongoengine.py
+++ test/test_mongoengine.py
@@ -4,17 +4,16 @@ from __future__ import unicode_literals
 
 import pytest
 
+from pymongo.collection import Collection
+from mongoengine import Document, StringField
+
 from web.core.context import Context
 from web.db.me import MongoEngineConnection
 from web.ext.db import DatabaseExtension
 
-from mongoengine import Document, StringField
-
-
 
 class Sample(Document):
 	name = StringField()
-
 
 
 @pytest.fixture
@@ -31,9 +30,29 @@ def db():
 	db.stop(ctx)
 
 
-
 class TestMongoEngineConnection(object):
 	def test_connection_lifecycle(self, db):
 		assert db._connection
-
+	
+	def test_private_is_protected(self, db):
+		with pytest.raises(AttributeError):
+			db._private
+		
+		with pytest.raises(KeyError):
+			db['_private']
+	
+	def test_collection_access(self, db):
+		assert isinstance(db.hello, Collection)
+		assert isinstance(db['hello'], Collection)
+	
+	def test_document_access(self, db):
+		assert db.Sample is Sample
+		assert db['Sample'] is Sample
+	
+	def test_missing_document(self, db):
+		with pytest.raises(AttributeError):
+			db.Example
+		
+		with pytest.raises(KeyError):
+			db['Example']
 
